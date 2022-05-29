b5649f96c92b6bdbd60e66abf4b62a8f15dc36a2
Aaron Reyes <ProdAronBeatz@gmail.com>
2022-05-28 23:39:12 -0500

    starting a new practice project

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import SingleColor from "./SingleColor";
+
+const App = () => {
+  return <div className="App">Hey Guys</div>;
+};
+
+export default App;
---
+++ src/SingleColor.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import rgbToHex from "./Utils";
+
+const SingleColor = () => {
+  return <h4>Single Color</h4>;
+};
+
+export default SingleColor;
