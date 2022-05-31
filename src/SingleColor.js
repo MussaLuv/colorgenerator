cb50f4b2d1b784b251eb8d5bea0ca98c91867008
Aaron Reyes <ProdAronBeatz@gmail.com>
2022-05-30 20:22:45 -0500

    Practiced how to use values js to get all the colors and how to work showing the data

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,8 +1,31 @@
 import React, { useState, useEffect } from "react";
 import rgbToHex from "./Utils";
 
-const SingleColor = () => {
-  return <h4>Single Color</h4>;
+const SingleColor = ({ rgb, weight, index, hexColor }) => {
+  const [alert, setAlert] = useState(false);
+  const bdg = rgb.join(",");
+  const hex = rgbToHex(...rgb);
+  const hexValue = `#${hexColor}`;
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, [alert]);
+  return (
+    <article
+      className={`color ${index > 10 && "color-light"}`}
+      style={{ backgroundColor: `rgb(${bdg})` }}
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hexValue);
+      }}
+    >
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">{hexValue}</p>
+      {alert && <p className="alert">Copied to clipboard</p>}
+    </article>
+  );
 };
 
 export default SingleColor;
